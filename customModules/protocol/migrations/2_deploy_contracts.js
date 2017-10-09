4c4b7c5ebbdde32f0c8bcbc3fb5ccbf3925d1cca
Janaka-Steph <rochestephane@yahoo.fr>
2017-10-09 11:37:10 +0100

    move contracts

--- customModules/protocol/migrations/2_deploy_contracts.js
+++ customModules/protocol/migrations/2_deploy_contracts.js
@@ -1,30 +1,32 @@
-var SafeMath = artifacts.require('../../../node_modules/zeppelin-solidity/contracts/math/SafeMath.sol')
+// Libraries
+//var SafeMath = artifacts.require('../../../node_modules/zeppelin-solidity/contracts/math/SafeMath.sol')
 
 var ConvertLib = artifacts.require('ConvertLib')
 var MetaCoin = artifacts.require('MetaCoin')
 var Greeter = artifacts.require('Greeter')
 var ProofOfExistence = artifacts.require('ProofOfExistence')
+
 // Standard Token
-var StandardToken = artifacts.require('StandardToken')
-var HumanStandardToken = artifacts.require('HumanStandardToken')
-var HumanStandardTokenFactory = artifacts.require('HumanStandardTokenFactory')
-var SampleRecipientSuccess = artifacts.require('SampleRecipientSuccess')
-var SampleRecipientThrow = artifacts.require('SampleRecipientThrow')
+//var StandardToken = artifacts.require('StandardToken')
+//var HumanStandardToken = artifacts.require('HumanStandardToken')
+//var HumanStandardTokenFactory = artifacts.require('HumanStandardTokenFactory')
+//var SampleRecipientSuccess = artifacts.require('SampleRecipientSuccess')
+//var SampleRecipientThrow = artifacts.require('SampleRecipientThrow')
 // ERC223 Token
-var ERC223 = artifacts.require('ERC223')
-var ERC223Token = artifacts.require('ERC223Token')
-var ContractReceiver = artifacts.require('ContractReceiver')
+//var ERC223 = artifacts.require('ERC223')
+//var ERC223Token = artifacts.require('ERC223Token')
+//var ContractReceiver = artifacts.require('ContractReceiver')
 // Simple TheDAO
-var SimpleTheDAO = artifacts.require('./contracts/SimpleTheDAO/SimpleTheDAO.sol')
-var Mallory = artifacts.require('./contracts/SimpleTheDAO/Mallory.sol')
-var Mallory2 = artifacts.require('./contracts/SimpleTheDAO/Mallory2.sol')
+//var SimpleTheDAO = artifacts.require('./contracts/SimpleTheDAO/SimpleTheDAO.sol')
+//var Mallory = artifacts.require('./contracts/SimpleTheDAO/Mallory.sol')
+//var Mallory2 = artifacts.require('./contracts/SimpleTheDAO/Mallory2.sol')
 
 module.exports = function (deployer) {
-  //deployer.deploy(ConvertLib)
-  //deployer.link(ConvertLib, MetaCoin)
-  //deployer.deploy(MetaCoin)
-  //deployer.deploy(Greeter)
-  //deployer.deploy(ProofOfExistence)
+  deployer.deploy(ConvertLib)
+  deployer.link(ConvertLib, MetaCoin)
+  deployer.deploy(MetaCoin)
+  deployer.deploy(Greeter)
+  deployer.deploy(ProofOfExistence)
   // Token standard
   //deployer.deploy(StandardToken)
   //deployer.deploy(HumanStandardToken)
